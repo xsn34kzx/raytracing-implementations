4458c897273b83c081657a0f8d70ee96c4f2dbee
xsn34kzx <xsn34kzx@gmail.com>
2024-05-20 15:35:05 -0400

    WIP (JAVA): Added Changed Files

--- Java/src/main/java/kz/sn34/raytrace-java-lib/Metal.java
+++ Java/src/main/java/kz/sn34/raytrace-java-lib/Metal.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 public class Metal extends Material implements Serializable
 {
+    private static final long serialVersionUID = 1L;
+
     private double fuzz;
 
     public Metal(Vector3 albedo, double fuzz)
